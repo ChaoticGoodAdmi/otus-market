782d6fce2c3d800c85c32e491a0a4be3fda2af17
Kir Ushakov <imonare@gmail.com>
2024-10-21 11:29:02 +0300

    Created Order Service

---
+++ src/main/kotlin/ru/ushakov/otusmarket/OtusMarketApplication.kt
@@ -0,0 +1,11 @@
+package ru.ushakov.otusmarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OtusMarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<OtusMarketApplication>(*args)
+}
---
+++ src/test/kotlin/ru/ushakov/otusmarket/OtusMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.ushakov.otusmarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OtusMarketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
